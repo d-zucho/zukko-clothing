21d6d188a6c96c2d10411396987e3929f4dac77a
D_Zucho <danny.nico.dev@outlook.com>
2021-10-24 17:25:01 -0700

    add withRouter higher order component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage'
+import { Switch, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div className='example'>
+    <h1>hats page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <Switch>
+        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   )
 }
